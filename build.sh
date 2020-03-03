95c1d00a5a443e97ed8567e94b9452fd83eca46e
Ola Brunborg Vikholt <ovikholt@gmail.com>
2020-03-03 19:17:12 +0100

    Improve build script instructions to developer

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir -p dist/coffee-to-js-output &&\
 coffee --compile --output dist/coffee-to-js-output src &&\
 browserify dist/coffee-to-js-output/content-script.js > dist/browserify-output/content-script.js &&\
-echo "yay."
+echo "yay." &&\
+echo "Now go to chrome://extensions/ and reload the extension."
