ae5dfde8648ea55612c5a36ddc0d3b3b72f11973
Ola Brunborg Vikholt <ovikholt@gmail.com>
2020-03-04 14:20:29 +0100

    Do a yarn install before building, to ease developing

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+yarn install &&\
 mkdir -p dist/coffee-to-js-output &&\
 coffee --compile --output dist/coffee-to-js-output src &&\
 browserify dist/coffee-to-js-output/content-script.js > dist/browserify-output/content-script.js &&\
